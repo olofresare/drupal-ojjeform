5b64df861367605e7501bf724ac87afae6127f14
olofresare <olof.resare@moremedia.se>
2014-09-30 15:03:35 +0200

    Added js and css files in module

---
+++ ojjeform/js/ojjeform.js
@@ -0,0 +1,23 @@
+(function ($, Drupal, window, document, undefined) {
+  
+  $(document).ready(function() {
+    
+    var $languageSwitcherForm = $('#language-switcher-form');
+
+    /* Style form components with jquery plugin */
+    if ($languageSwitcherForm.length > 0) {
+      var formOptions = {
+        types: {
+          checkbox: false,
+          radio: false,
+          select: true,
+          textfield: false,
+          textarea: false
+        }
+      };
+      $.ojjeform($languageSwitcherForm, formOptions);
+    }
+    
+  });
+
+})($jq || jQuery, Drupal, this, this.document);
